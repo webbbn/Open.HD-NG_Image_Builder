bc25a8ce90204d01a06dc3c48f75bc098db7ad37
Brian Webb <webbbn@gmail.com>
2019-12-06 20:25:37 +0000

    Reverses the order of the openhd legacy package scripts and adds an option to run_step to just bring up a chrooted shell.

--- steps/00-chroot-install-packages.sh
+++ steps/00-chroot-install-packages.sh
@@ -5,6 +5,19 @@ rm /etc/ld.so.preload
 
 # Install dependencies
 apt-get update
-apt-get install -y python3-pyudev libpcap0.8 python3-serial python3-pip cython3 libv4l-dev libboost-all-dev python3-numpy
+apt-get install -y \
+	python3-pyudev \
+	libpcap0.8-dev \
+	libpcap0.8 \
+	python3-serial \
+	python3-pip \
+	cython3 \
+	libv4l-dev \
+	libboost-all-dev \
+	python3-numpy \
+	indent \
+	git \
+	libsodium-dev \
+	wiringpi
 pip3 install pyric
 pip3 install pymavlink
