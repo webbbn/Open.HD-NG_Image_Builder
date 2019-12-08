f8f1c904cec9e3ee41c4b6a163b3c35b96f6edb7
Brian Webb <bnw@rincon.com>
2019-12-08 14:34:52 +0000

    Fixes configuration of the pi camera.

--- steps/20-system-setup.sh
+++ steps/20-system-setup.sh
@@ -12,9 +12,12 @@ echo "enable_uart=1" >> ${IMAGE_ROOT}/boot/config.txt
 sed -i 's/console[^ ]*//g' ${IMAGE_ROOT}/boot/cmdline.txt
 
 # Enable the camera
-sed -i 's/start_x=0/start_x=1/g' ${IMAGE_ROOT}/boot/config.txt
+sed -i 's/start_x/d' ${IMAGE_ROOT}/boot/config.txt
+sed -i 's/gpu_mem/d' ${IMAGE_ROOT}/boot/config.txt
+sed -i 's/disable_camera_led/d' ${IMAGE_ROOT}/boot/config.txt
+echo "start_x=1" >> ${IMAGE_ROOT}/boot/config.txt
 echo "gpu_mem=128" >> ${IMAGE_ROOT}/boot/config.txt
-echo " disable_camera_led=1" >> ${IMAGE_ROOT}/boot/config.txt
+echo "disable_camera_led=1" >> ${IMAGE_ROOT}/boot/config.txt
 
 # Change the OpenHD UART to /dev/ttyS0
 sed -i 's/ttyS1/ttyS0/g' ${IMAGE_ROOT}/etc/default/openhd
