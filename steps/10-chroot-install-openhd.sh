6254988b89b23df308090e6381ed148b438d6654
Brian Webb <bnw@rincon.com>
2019-12-08 21:14:53 +0000

    Updates openhd package versions.

--- steps/10-chroot-install-openhd.sh
+++ steps/10-chroot-install-openhd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Install thw wifibroadcast_bridge package
-wget -O deb-files.zip https://github.com/webbbn/wifibroadcast_bridge/suites/347161937/artifacts/591135
+wget -O deb-files.zip https://github.com/webbbn/wifibroadcast_bridge/suites/347975217/artifacts/598834
 unzip deb-files.zip
 dpkg -i deb-files/buster-armhf/wifibroadcast_bridge_0.1.0_armhf.deb
 
 # Install the openhd package
-wget -O deb-files.zip https://github.com/webbbn/Open.HD-NG/suites/346775428/artifacts/587601
+wget -O deb-files.zip https://github.com/webbbn/Open.HD-NG/suites/348012003/artifacts/599349
 unzip deb-files.zip
 dpkg -i deb-files/buster-armhf/open.hd-ng_0.1.0_armhf.deb
 
