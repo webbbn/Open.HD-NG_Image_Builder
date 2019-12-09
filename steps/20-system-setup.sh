929a5effa7e1d0f3ab7bc0c3b982159ed01fb92a
Brian Webb <webbbn@gmai.com>
2019-12-09 05:11:19 +0000

    Adds some additional configuration options to prevent system conflicts

--- steps/20-system-setup.sh
+++ steps/20-system-setup.sh
@@ -7,17 +7,24 @@ cp -r files/overlay/* ${IMAGE_ROOT}
 sed -i 's/raspberrypi/openhd-ng/g' ${IMAGE_ROOT}/etc/hosts
 sed -i 's/raspberrypi/openhd-ng/g' ${IMAGE_ROOT}/etc/hostname
 
+# Disable bluetooth. Bluetooth makes the systemd-udev daemon use CPU on the zero
+echo "dtoverlay=disable-bt" >> ${IMAGE_ROOT}/boot/config.txt
+
 # Turn on the UART
+sed -i 's/enable_uart/d' ${IMAGE_ROOT}/boot/config.txt
 echo "enable_uart=1" >> ${IMAGE_ROOT}/boot/config.txt
 sed -i 's/console[^ ]*//g' ${IMAGE_ROOT}/boot/cmdline.txt
 
 # Enable the camera
 sed -i 's/start_x/d' ${IMAGE_ROOT}/boot/config.txt
-sed -i 's/gpu_mem/d' ${IMAGE_ROOT}/boot/config.txt
-sed -i 's/disable_camera_led/d' ${IMAGE_ROOT}/boot/config.txt
 echo "start_x=1" >> ${IMAGE_ROOT}/boot/config.txt
+sed -i 's/gpu_mem/d' ${IMAGE_ROOT}/boot/config.txt
 echo "gpu_mem=128" >> ${IMAGE_ROOT}/boot/config.txt
+sed -i 's/disable_camera_led/d' ${IMAGE_ROOT}/boot/config.txt
 echo "disable_camera_led=1" >> ${IMAGE_ROOT}/boot/config.txt
 
 # Change the OpenHD UART to /dev/ttyS0
 sed -i 's/ttyS1/ttyS0/g' ${IMAGE_ROOT}/etc/default/openhd
+
+# Disable most automatic network configuration functions, which can interfere with the wifi devices.
+echo -e "auto lo\niface lo inet loopback\nauto eth0\nallow-hotplug eth0\niface eth0 inet manual\niface wlan0 inet manual\niface wlan1 inet manual\niface wlan2 inet manual" ${IMAGE_ROOT}/etc/network/interfaces
