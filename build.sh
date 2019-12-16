ef7e49c8c088fe3b733e7e27276bc5773174e44f
Brian Webb <bnw@rincon.com>
2019-12-16 06:27:23 +0000

    Adds installation of QOpenHD.

--- build.sh
+++ build.sh
@@ -5,64 +5,19 @@ set -e
 # Source the config file
 . config
 
-# Download the raspberry pi image if necessary
-if [ ! -f ${DL_IMAGE} ]; then
-    echo "Downloading Raspbian image"
-    wget ${BASE_IMAGE_URL}
-    unzip ${BASE_IMAGE}.zip
-fi
-
-# Create a larger image
-echo "Creating the initial image from the downloaded copy and expanding it"
-fallocate -l 3500M ${IMAGE_NAME}
-dd conv=notrunc if=${DL_IMAGE} of=${IMAGE_NAME}
-
-# Resize the root partition
-echo "Resizing the root partion of: ${IMAGE_NAME}"
-parted --script ${IMAGE_NAME} resizepart 2 100%
-
-# Connect the image to a loopback device
-LOOP=`losetup -P -f --show ${IMAGE_NAME}`
-
-# Resize the root filesystem
-echo "Checking the root partition and resizing it"
-e2fsck -y -f ${LOOP}p2
-resize2fs ${LOOP}p2 3200M
-if [ ! -d ${IMAGE_ROOT} ]; then
-    mkdir -p ${IMAGE_ROOT}
-fi
-if [ -d "${STAGE_WORK_DIR}" ]; then
-    rm -rf "${STAGE_WORK_DIR}"
-fi
-mkdir "${STAGE_WORK_DIR}"
-
-# Mount the root and boot partition
-echo "Mounting the partitions"
-mount ${LOOP}p2 ${IMAGE_ROOT}
-mount ${LOOP}p1 ${IMAGE_ROOT}/boot
-
-# Copy the qemu binary into the root directory so we can run it when we chroot
-cp /usr/bin/qemu-arm-static ${IMAGE_ROOT}/usr/bin
+# Download the image of necessary
+${BUILD_ROOT}/scripts/download_image
 
 # Loop through all the build steps
 for SCRIPT in steps/*.sh; do
-    ./run_step ${SCRIPT}
+    ${BUILD_ROOT}/scripts/run_step ${SCRIPT}
 done
-rm -rf ${IMAGE_ROOT}/tmp/*
 
-# Unmount the partitions
-umount ${LOOP}p1
-umount ${LOOP}p2
-losetup -d ${LOOP}
+# Shrink the image to reduce the size
+${BUILD_ROOT}/scripts/shrink
 
-# Run pishrink on the image to reduce the size
-./pishrink.sh "${IMAGE_NAME}"
-
-# Zipping and moving the image to the deploy directory
-if [ ! -d deploy ]; then
-    mkdir deploy
-fi
-zip deploy/"${IMAGE_NAME}.zip" "${IMAGE_NAME}"
+# Deploy the image
+${BUILD_ROOT}/scripts/deploy
 
 echo "Image creation complete:"
 ls -lh "deploy/${IMAGE_NAME}.zip"
--- steps/00-chroot-install-packages.sh
+++ steps/00-chroot-install-packages.sh
@@ -21,8 +21,24 @@ apt-get install -y \
 	git \
 	libsodium-dev \
 	wiringpi \
-	omxplayer
+	omxplayer \
+	libdouble-conversion1 \
+	libgstreamer-gl1.0-0 \
+	libpcre2-dev \
+	libmtdev1 \
+	libinput10 \
+	libts0 \
+	libxkbcommon0
 apt-get remove -y \
 	firmware-atheros
 pip3 install pyric
 pip3 install pymavlink
+
+# Update the GL libraries to fix GLES support
+rm /usr/lib/arm-linux-gnueabihf/libEGL.so.1*
+sudo ln -s /opt/vc/lib/libbrcmEGL.so /opt/vc/lib/libEGL.so
+sudo ln -s /usr/lib/arm-linux-gnueabihf/libEGL.so.1.1.0 /usr/lib/arm-linux-gnueabihf/libEGL.so.1.1
+sudo ln -s /usr/lib/arm-linux-gnueabihf/libEGL.so.1.1 /usr/lib/arm-linux-gnueabihf/libEGL.so.1
+sudo ln -s /usr/lib/arm-linux-gnueabihf/libEGL.so.1 /usr/lib/arm-linux-gnueabihf/libEGL.so
+sudo ln -s /opt/vc/lib/libGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0
+sudo ln -s /opt/vc/lib/libbrcmGLESv2.so /opt/vc/lib/libGLESv2.so
