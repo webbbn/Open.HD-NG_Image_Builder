5be8df761352b3a7926b9316f2cc3e7b3023a3aa
Brian Webb <webbbn@gmai.com>
2019-12-17 00:30:28 +0000

    Updates the wifibroadcast_bridge and openhd-hg versions, and performs some more cleanup to reduce the image size and to eliminate unecessary services.

--- steps/10-chroot-install-openhd.sh
+++ steps/10-chroot-install-openhd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install thw wifibroadcast_bridge package
-wget -O deb-files.zip https://github.com/webbbn/wifibroadcast_bridge/suites/359215697/artifacts/700392
+wget -O deb-files.zip https://github.com/webbbn/wifibroadcast_bridge/suites/361444424/artifacts/719885
 unzip deb-files.zip
 dpkg -i deb-files/buster-armhf/wifibroadcast_bridge_*.deb
 rm -rf deb-files*
 
 # Install the openhd package
-wget -O deb-files.zip https://github.com/webbbn/Open.HD-NG/suites/359793993/artifacts/705593
+wget -O deb-files.zip https://github.com/webbbn/Open.HD-NG/suites/361530243/artifacts/720493
 unzip deb-files.zip
 dpkg -i deb-files/buster-armhf/open.hd-ng_*.deb
 rm -rf deb-files*
